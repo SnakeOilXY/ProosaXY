c9591fda9f401a3af2e5eff7c4ff1b866bece09a
cupradito <jfeito.movil@gmail.com>
2026-04-24 20:35:16 +0200

    proosa mini st7789v display config docs and files

---
+++ Mods/ProosaXY Mini/config/display/logo.py
@@ -0,0 +1,19 @@
+# Utility to read and resize the Klipper logo.
+#
+# Copyright (C) 2021  Matthew Lloyd <github@matthewlloyd.net>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import os
+from PIL import Image
+
+LOGO_IMAGE = None
+
+def make_logo(width):
+    global LOGO_IMAGE
+    if LOGO_IMAGE is None:
+        LOGO_IMAGE = Image.open(
+            os.path.join(os.path.dirname(__file__),
+                         './logo.png'))
+    new_height = int(LOGO_IMAGE.height / float(LOGO_IMAGE.width) * width)
+    return LOGO_IMAGE.resize((width, new_height), resample=Image.LANCZOS)
+
